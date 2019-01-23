e3323e35af03e71649c3724837c50054a91a114d
gshokar <gshokar@yahoo.com>
2019-01-22 20:26:40 -0500

    Add the sequence for Client Location

--- source/bl/dbscripts/Invoicebook_Dev.sql
+++ source/bl/dbscripts/Invoicebook_Dev.sql
@@ -18,3 +18,5 @@ INSERT INTO addresstype (TypeId, `Name`, GUID, AddedBy, AddedDate, LastUpdatedBy
 INSERT INTO contacttype (TypeId, `Name`, GUID, AddedBy, AddedDate, LastUpdatedBy, LastUpdatedDate) VALUES(1, 'Business', UPPER(UUID()), 0, Now(), 0, Now());
 
 INSERT INTO `tblsequence`(SequenceId, Filler, IdGenerator, LastNo, MaxLength, `Name`, PostFixString, PreFixString) VALUES (1,'0',NULL,100000,6,'ClientNumber',NULL,NULL);
+INSERT INTO `tblsequence`(SequenceId, Filler, IdGenerator, LastNo, MaxLength, `Name`, PostFixString, PreFixString) VALUES (2,'0',NULL,100000,6,'EmployeeNumber',NULL,NULL);
+INSERT INTO `tblsequence`(SequenceId, Filler, IdGenerator, LastNo, MaxLength, `Name`, PostFixString, PreFixString) VALUES (3,'0',NULL,100000,6,'ClientLocationNumber',NULL,NULL);
